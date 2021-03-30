c5c3d0f01286d97b2e037b54e1ba072273db5d93
Danilo Argentiero <argentiero.danilo@gmail.com>
2021-03-30 11:23:12 +0200

    init: first commit #working

---
+++ Model/Config.php
@@ -0,0 +1,46 @@
+<?php
+/*
+ * Copyright © Websolute spa. All rights reserved.
+ * See LICENSE and/or COPYING.txt for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Websolute\TransporterExporter\Model;
+
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Store\Model\ScopeInterface;
+
+class Config
+{
+    const EXPORT_IS_ENABLED_CONFIG_PATH = 'transporter_exporter/general/enabled';
+
+    /**
+     * @var ScopeConfigInterface
+     */
+    private $scopeConfig;
+
+    /**
+     * @var \Websolute\TransporterBase\Model\Config
+     */
+    private $baseConfig;
+
+    public function __construct(
+        ScopeConfigInterface $scopeConfig,
+        \Websolute\TransporterBase\Model\Config $baseConfig
+    ) {
+        $this->scopeConfig = $scopeConfig;
+        $this->baseConfig = $baseConfig;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isEnabled(): bool
+    {
+        return $this->baseConfig->isEnabled() && (bool)$this->scopeConfig->getValue(
+            self::EXPORT_IS_ENABLED_CONFIG_PATH,
+            ScopeInterface::SCOPE_STORE
+        );
+    }
+}
